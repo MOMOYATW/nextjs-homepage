87276b253aafe1952c823436741958974eace391
Ye Tao <davytao@buaa.edu.cn>
2023-01-09 00:49:01 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/nextjs-homepage/" : "",
+  assetPrefix: isProd ? "" : "",
   images: {
     unoptimized: true,
   },
